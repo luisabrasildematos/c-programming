336152e14a1202c6dab9b15dba25f789fba30f00
Luisa Brasil <luisabrasildematos@gmail.com>
2025-04-02 03:09:28 -0300

    Upload pointers-2.c to april2025

---
+++ april2025/pointers-2.c
@@ -0,0 +1,19 @@
+
+//find out lenght of string
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+char oxygen[] = "oxygen";
+char *p = oxygen;
+
+int length = strlen(oxygen);
+
+printf("Length of string: %d\n", length);
+
+return 0;
+
+}
+
